ce99780ead2593c512ef39fc600c51e306617cd8
sudeep040 <sudeep@upliance.ai>
2025-03-21 15:52:39 +0530

    changes

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -20,7 +20,7 @@ app.use(compression());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: '*',
     credentials: true,
   })
 );
--- packages/frontend/utils/config.ts
+++ packages/frontend/utils/config.ts
@@ -1,5 +1,5 @@
-const API_PRODUCTION_URL = 'http://localhost:4000';
-const API_DEV_URL = 'http://localhost:4000';
+const API_PRODUCTION_URL = 'https://community-app-oe60.onrender.com';
+const API_DEV_URL = 'https://community-app-oe60.onrender.com/';
 
 export const Config = {
   API_URL: process.env.NODE_ENV === 'production' ? API_PRODUCTION_URL : API_DEV_URL,
